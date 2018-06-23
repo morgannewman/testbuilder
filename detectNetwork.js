5415dcd5e7b5f427855ce4976e4f2b6764464015
Morgan Newman <mpnewman95@gmail.com>
2018-06-23 12:35:24 -0700

    Initial commit

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,8 +11,35 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  
+  // Create string representation of first two digits of cardNumber  
+  let firstTwoDigits = 
+    cardNumber
+    .toString()
+    .slice(0,2);
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+  // Check Diner Club length and prefix
+  let isDinersClub = 
+    (firstTwoDigits === '38' || firstTwoDigits === '39')
+    &&
+    cardNumber.length === 14;
+  
+  if (isDinersClub) {
+    return 'Diner\'s Club';
+  }
+  
+  // Check AE length and prefix
+  let isAmericanExpress = 
+    (firstTwoDigits === '34' || firstTwoDigits === '37')
+    &&
+    cardNumber.length === 15;
+
+  if (isAmericanExpress) {
+    return 'American Express';
+  }
 };
 
-
+console.log(detectNetwork('38345678901234'));
+console.log(detectNetwork('39345678901234'));
+console.log(detectNetwork('343456789012345'));
+console.log(detectNetwork('373456789012345'));
